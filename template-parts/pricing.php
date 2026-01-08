a24773bd3a53cc6a792a4937baf1908155935c49
Michael Kanios <mkanios77@gmail.com>
2026-01-08 10:43:03 +0000

    Final

--- template-parts/pricing.php
+++ template-parts/pricing.php
@@ -1,15 +1,13 @@
 <?php
-$title = get_field('pricing_title');
-$cards = get_field('pricing_cards');
+$title = get_field('pricing_title'); // ACF free
 ?>
 
 <section
-  id="pricing-react"
-  data-props='<?php echo json_encode([
-    "title" => $title,
-    "cards" => $cards
-  ]); ?>'
-  class="my-24"
+    id="hybrid-react"
+    data-props='<?php echo json_encode([
+        "title" => $title,
+    ]); ?>'
+    class="my-20"
 >
-  <!-- React κάνει mount εδώ μέσω JS -->
+    <!-- React θα κάνει mount εδώ -->
 </section>
